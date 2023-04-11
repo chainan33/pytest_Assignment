cc2f1591ef73617fa91f54d716d86bb67b3beb19
chainan33 <126548574+chainan33@users.noreply.github.com>
2023-04-11 23:35:07 +0700

    Update test_function.py

--- final/test_function.py
+++ final/test_function.py
@@ -25,35 +25,6 @@ def test_calculateMonth_case6():
     #เกินเดือน มีเลทงาน
     assert calculateMonth(31, 0, 1) == 10540
 
-# import unittest
-# from function import calculate_salary
-
-# class TestCalculateSalary(unittest.TestCase):
-#     def test_calculate_salary_no_ot_no_late(self):
-#         self.assertAlmostEqual(calculate_salary(22, 0, 0), 7480.00)
-
-#     def test_calculate_salary_with_ot_no_late(self):
-#         self.assertAlmostEqual(calculate_salary(22, 2, 0), 8380.00)
-
-#     def test_calculate_salary_no_ot_with_late(self):
-#         self.assertAlmostEqual(calculate_salary(20, 0, 2), 6240.00)
-
-#     def test_calculate_salary_with_ot_and_late(self):
-#         self.assertAlmostEqual(calculate_salary(20, 2, 2), 7140.00)
-
-#     def test_calculate_salary_with_max_ot_and_no_late(self):
-#         self.assertAlmostEqual(calculate_salary(22, 3, 0), 8740.00)
-
-#     def test_calculate_salary_with_max_ot_and_late(self):
-#         self.assertAlmostEqual(calculate_salary(20, 3, 2), 7540.00)
-
-#     def test_calculate_salary_with_more_than_max_ot(self):
-#         self.assertRaises(ValueError, calculate_salary, 22, 4, 0)
-
-#     def test_calculate_salary_with_negative_values(self):
-#         self.assertRaises(ValueError, calculate_salary, -1, 2, 0)
-#         self.assertRaises(ValueError, calculate_salary, 22, -1, 0)
-#         self.assertRaises(ValueError, calculate_salary, 22, 2, -1)
-
-# if __name__ == '__main__':
-#     unittest.main()
+def test_calculateMonth_case7():
+    #เกินเดือน OT เกิน
+    assert calculateMonth(31, 4, 0) == 11380
